8600c73c009ea8745f108be9f9c2cd3ff6bab47a
Renan Alexandre <renanalexandre260@gmail.com>
2018-10-29 01:07:30 -0300

    Video View running

--- ProjetoLibDowload/app/src/main/java/br/edu/fbv/projetolibdowload/MainActivity.java
+++ ProjetoLibDowload/app/src/main/java/br/edu/fbv/projetolibdowload/MainActivity.java
@@ -1,13 +1,29 @@
 package br.edu.fbv.projetolibdowload;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button botaovideo ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaovideo = findViewById(R.id.botaovideo);
+
+        botaovideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReproducaoVideoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
