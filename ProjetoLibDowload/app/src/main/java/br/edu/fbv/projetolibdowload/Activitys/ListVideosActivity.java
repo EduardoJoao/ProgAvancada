3b70ae2f15b3fe581fd5ec0fe62aa5cb11f02527
Renan Alexandre <renanalexandre260@gmail.com>
2018-11-13 01:50:36 -0200

    Activity com listagem dos dowloads

---
+++ ProjetoLibDowload/app/src/main/java/br/edu/fbv/projetolibdowload/Activitys/ListVideosActivity.java
@@ -0,0 +1,15 @@
+package br.edu.fbv.projetolibdowload.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.fbv.projetolibdowload.R;
+
+public class ListVideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_videos);
+    }
+}
