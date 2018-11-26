a9022676890386b45d34ebc4937a5f07093a50c7
Renan Alexandre <renanalexandre260@gmail.com>
2018-11-26 21:08:41 -0200

    Commit Lib

--- ProjetoLibDowload/app/src/main/java/br/edu/fbv/projetolibdowload/Activitys/ListVideosActivity.java
+++ ProjetoLibDowload/app/src/main/java/br/edu/fbv/projetolibdowload/Activitys/ListVideosActivity.java
@@ -3,9 +3,15 @@ package br.edu.fbv.projetolibdowload.Activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import br.edu.fbv.projetolibdowload.R;
 
+
+
+
+
 public class ListVideosActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
